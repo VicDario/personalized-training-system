f6666320c99d5d880a810c8362ac75e01672b6b9
VicDario <26585356+VicDario@users.noreply.github.com>
2026-03-29 19:21:23 -0300

    feat: add base exercise class with getters and setters

---
+++ excercise.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include "exercise.h"
+
+Exercise::Exercise(int id, string name, string description, int intensity, int durationMinutes) {
+    this->id = id;
+    this->name = name;
+    this->description = description;
+    this->intensity = intensity;
+    this->durationMinutes = durationMinutes;
+}
+
+// Getters
+int Exercise::getId() const {
+    return id;
+}
+
+string Exercise::getName() const {
+    return name;
+}
+
+string Exercise::getDescription() const {
+    return description;
+}
+
+int Exercise::getIntensity() const {
+    return intensity;
+}
+
+int Exercise::getDurationMinutes() const {
+    return durationMinutes;
+}
+
+// Setters
+void Exercise::setId(int id) {
+    this->id = id;
+}
+
+void Exercise::setName(string name) {
+    this->name = name;
+}
+
+void Exercise::setDescription(string description) {
+    this->description = description;
+}
+
+void Exercise::setIntensity(int intensity) {
+    this->intensity = intensity;
+}
+
+void Exercise::setDurationMinutes(int durationMinutes) {
+    this->durationMinutes = durationMinutes;
+}
---
+++ exercise.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Exercise {
+    private:
+        int id;
+        string name;
+        string description;
+        int intensity;
+        int durationMinutes;
+
+    public:
+        Exercise(int id, string name, string description, int intensity, int durationMinutes);
+
+        // Getters
+        int getId() const;
+        string getName() const;
+        string getDescription() const;
+        int getIntensity() const;
+        int getDurationMinutes() const;
+
+        // Setters
+        void setId(int id);
+        void setName(string name);
+        void setDescription(string description);
+        void setIntensity(int intensity);
+        void setDurationMinutes(int durationMinutes);
+};
