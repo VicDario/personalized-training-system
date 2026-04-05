4155799b2feae926b70e93847dbe00574222ebfd
VicDario <26585356+VicDario@users.noreply.github.com>
2026-04-05 01:08:18 -0400

    feat: add helper class for shared methods

---
+++ helper.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "exercise-intensity.h"
+
+using namespace std;
+
+class Helper {
+public:
+    static ExerciseIntensity getExerciseIntensityFromUser();
+    static string getIntensityName(ExerciseIntensity intensity);
+};
